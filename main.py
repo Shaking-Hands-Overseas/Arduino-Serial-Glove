eb6e032c85f146e595daf4b2df9b9006d40a9855
Gondoraragorn <Joel.garcia1202@hotmail.com>
2021-11-20 21:14:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+"""
+Author : Joel Garcia (@Newtoniano20 / Newtoniano#1173 on discord)
+This code was created for the Project Shaking Hands Overseas with the purpouse of moving a hand from the other side
+of the ocean. Any questions feel free to ask.
+"""
+
 from requests.models import Response
 from serial import Serial
 from requests import post
+from time import sleep
 
 # SERIAL PORT: Windows = COM1, COM2, ... / Linux = /dev/ttyACM0, /dev/ttyACM1,...
 SERIAL_PORT = "COM3"
@@ -12,6 +19,7 @@ URL = 'https://lqinkt.deta.dev/servo'
 BAUDRATE = 9600
 
 def arduino_read():
+    ser.write(bytes("A", 'utf-8'))
     return ser.readline() 
 
 def split_data(data):
@@ -28,14 +36,15 @@ try:
 except:
     raise Exception(f"[ERROR] Arduino not connected to Serial port {SERIAL_PORT}")
 
+sleep(0.5) 
 while True:
     try:
-        #reads the data from the arduino 
-        arduino_read()
+        #reads the data from the arduino
+        data = split_data(arduino_read())
+        print(data)
     except:
         print(f"[ERROR] Arduino not connected to port {SERIAL_PORT}")
         ser = arduino_connect()
-    data = [0, 1, 2, 3, 4]
     post_request = { "s1": int(data[0]), "s2": int(data[1]), "s3": int(data[2]), "s4": int(data[3]),"s5": int(data[4]) }
     #sends the data to our API and prints the answser. If the response is 200, it means the deta was sent succesfull
     try:
