fd02a42ca5aedc033c2484674b96cda94ab7d523
Gondoraragorn <Joel.garcia1202@hotmail.com>
2021-12-23 19:22:28 +0100

    Improved Documentation

--- main.py
+++ main.py
@@ -15,43 +15,66 @@ SERIAL_PORT = "COM6"
 # API URL FOR SENDING DATA
 URL = 'https://xlbi6e.deta.dev/servo'
 
-#ARDUINO SERIAL BAUDRATE
+# ARDUINO SERIAL BAUDRATE
 BAUDRATE = 9600
 
+
 def arduino_read():
+    """
+    Reads data from arduino
+    :return: String
+    """
     ser.write(bytes("A", 'utf-8'))
     return ser.readline() 
 
-def split_data(data):
-    return data.split()
+
+def split_data(raw_data: str):
+    """
+    Splits data into a list
+    :param raw_data: String
+    :return: List
+    """
+    return raw_data.split()
+
 
 def arduino_connect():
-    return Serial( SERIAL_PORT , BAUDRATE) #Initializes the connection with the arduino, you will need to chose your serial port
+    """
+    Initializes the connection with the arduino, you will need to chose your serial port
+    :return: Serial
+    """
+    return Serial(SERIAL_PORT, BAUDRATE)
 
-def Post_req(URL, post_request):
-    return post(url=URL, json=post_request)
+
+def post_req(url, req):
+    return post(url=url, json=req)
+
 
 try:
     ser = arduino_connect()
+
 except:
     raise Exception(f"[ERROR] Arduino not connected to Serial port {SERIAL_PORT}")
 
 sleep(0.5) 
 while True:
     try:
-        #reads the data from the arduino
+        """
+        Reads the data from the arduino
+        """
         data = split_data(arduino_read())
         print(data)
     except:
         print(f"[ERROR] Arduino not connected to port {SERIAL_PORT}")
         ser = arduino_connect()
     try:
-        post_request = { "s1": int(data[0]), "s2": int(data[1]), "s3": int(data[2]), "s4": int(data[3]),"s5": int(data[4]) }
+        post_request = {"s1": int(data[0]), "s2": int(data[1]), "s3": int(data[2]), "s4": int(data[3]),"s5": int(data[4])}
     except:
         pass
-    #sends the data to our API and prints the answser. If the response is 200, it means the deta was sent succesfull
     try:
-        Req_Response = Post_req(URL=URL, post_request=post_request)
+        """
+        Sends the data to our API and prints the answser. If the response is 200, it means the deta was sent succesfullq
+        """
+        Req_Response = post_req(url=URL, req=post_request)
         print(Req_Response)
     except:
         print(f"[ERROR] Server {URL} not responding to request")
